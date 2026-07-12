ef51560fbde9f06947ca148fe0813cfe77ff0a53
Quangribon <quanglinhchau68@gmail.com>
2026-07-12 21:58:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ setInterval(() => {
 
 // ===== DATA =====
 const data = [
-  { name: "AP3 Hàng Cũ", price: 169000, oldPrice: 450000, cat: "ear", img: "anh/ap3.jpg", desc: "Tai nghe hàng cũ,đã qua sửa chữa." },
+  { name: "AP3 Hàng Cũ", price: 169000, oldPrice: 450000, cat: "ear", img: "anh/ap3.jpg", desc: "Tai nghe hàng cũ,đã qua sửa chữa.Không áp dụng giảm giá" },
   { name: "AP4 ANC", price: 650000, cat: "ear", img: "anh/ap4.jpg", desc: "Tai nghe chống ồn ANC, pin 8h, case 30h" },
-  { name: "AP-PRO Hàng Cũ ", price: 199000, cat: "ear", img: "anh/appro.jpg", desc: "Tai nghe hàng cũ,đã qua sửa chữa." },
+  { name: "AP-PRO Hàng Cũ ",oldPrice: 450000, price: 199000, cat: "ear", img: "anh/appro.jpg", desc: "Tai nghe hàng cũ,đã qua sửa chữa.Không áp dụng giảm giá" },
   { name: "AP-PRO2 ANC", price: 650000, cat: "ear", img: "anh/pro2.jpg", desc: "Tai nghe chống ồn ANC, pin 8h, case 30h" },
   { name: "AP-PRO3 ANC", price: 950000, cat: "ear", img: "anh/pro3.jpg", desc: "Tai nghe cao cấp nhất" },
   { name: "Chống Rớt Tai Nghe", price: 35000, cat: "acc", img: "anh/phu kien chong roi.jpg", desc: "Giữ tai nghe giúp tự tin hơn khi chạy bộ"},
